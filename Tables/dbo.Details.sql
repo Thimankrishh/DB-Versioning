040220845af04085e3c1c7057ff8e198d56d5048
Thiman Krishly <thiman.krishly@sprintlabs.lk>
2020-08-10 08:15:52 +0530

    init

---
+++ Tables/dbo.Details.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Details] (
+  [id] [int] IDENTITY,
+  [name] [varchar](50) NOT NULL,
+  [age] [varchar](50) NOT NULL,
+  CONSTRAINT [PK_Details_id] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO

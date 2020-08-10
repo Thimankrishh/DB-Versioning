c3856fee2c4c43a3e51f86fb9e3fa229366c3ee1
Thimankrishh <32634400+Thimankrishh@users.noreply.github.com>
2020-08-10 10:10:34 +0530

    update data type of age

--- Tables/dbo.Details.sql
+++ Tables/dbo.Details.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Details] (
   [id] [int] IDENTITY,
   [name] [varchar](50) NOT NULL,
-  [age] [varchar](50) NOT NULL,
+  [age] [int](50) NOT NULL,
   [university] [varchar](50) NOT NULL,
   [school] [varchar](50) NULL,
   CONSTRAINT [PK_Details_id] PRIMARY KEY CLUSTERED ([id])

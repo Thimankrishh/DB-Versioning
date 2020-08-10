788bd97ee6c87c78e24407d74ff529d9a0bc51bc
Thiman Krishly <thiman.krishly@sprintlabs.lk>
2020-08-10 08:21:31 +0530

    university column added

--- Tables/dbo.Details.sql
+++ Tables/dbo.Details.sql
@@ -2,6 +2,7 @@
   [id] [int] IDENTITY,
   [name] [varchar](50) NOT NULL,
   [age] [varchar](50) NOT NULL,
+  [university] [varchar](50) NOT NULL,
   CONSTRAINT [PK_Details_id] PRIMARY KEY CLUSTERED ([id])
 )
 ON [PRIMARY]

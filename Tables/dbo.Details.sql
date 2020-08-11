66f07ddfc81279c2e43268a085857fa86f0135d5
Thiman Krishly <thiman.krishly@sprintlabs.lk>
2020-08-11 20:30:40 +0530

    grade column added

--- Tables/dbo.Details.sql
+++ Tables/dbo.Details.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[Details] (
   [id] [int] IDENTITY,
   [name] [varchar](50) NOT NULL,
-  [age] [int](50) NOT NULL,
+  [age] [int] NOT NULL,
   [university] [varchar](50) NOT NULL,
   [school] [varchar](50) NULL,
+  [grade] [varchar](2) NOT NULL DEFAULT (1),
   CONSTRAINT [PK_Details_id] PRIMARY KEY CLUSTERED ([id])
 )
 ON [PRIMARY]

55e9cf88fb852c8c4161ed6ee616439d746ced4f
Thiman Krishly <thiman.krishly@sprintlabs.lk>
2020-08-11 22:23:32 +0530

    data insert

---
+++ Tables/StaticData/dbo.Details.Data.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT dbo.Details ON
+GO
+INSERT dbo.Details(id, name, age, university, school, grade) VALUES (1, 'hulk', 23, 'uom', NULL, '1')
+INSERT dbo.Details(id, name, age, university, school, grade) VALUES (2, 'saman', 23, 'UoC', NULL, '1')
+INSERT dbo.Details(id, name, age, university, school, grade) VALUES (3, 'kasun', 32, 'UoK', NULL, '1')
+GO
+SET IDENTITY_INSERT dbo.Details OFF
+GO
